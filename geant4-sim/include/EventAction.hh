eaaa7c810abce03dcd46280409b8122a07eb3aa1
muslu <musluoglu.mert10@gmail.com>
2025-04-05 19:22:37 +0300

    Update EventAction.hh

--- geant4-sim/include/EventAction.hh
+++ geant4-sim/include/EventAction.hh
@@ -1,1 +1,17 @@
+#ifndef EVENT_ACTION_HH
+#define EVENT_ACTION_HH
+
+#include "G4UserEventAction.hh"
+class RunAction;
+
+class EventAction : public G4UserEventAction {
+public:
+    EventAction(RunAction* runAction);
+    RunAction* GetRunAction() const;
+
+private:
+    RunAction* fRunAction;
+};
+
+#endif
 
