3764ea7b8618933b79fa8994a972d6ef74ee7bef
muslu <musluoglu.mert10@gmail.com>
2025-04-05 19:21:58 +0300

    Update ActionInitialization.cc

--- geant4-sim/src/ActionInitialization.cc
+++ geant4-sim/src/ActionInitialization.cc
@@ -1,1 +1,15 @@
+#include "ActionInitialization.hh"
+#include "PrimaryGeneratorAction.hh"
+#include "RunAction.hh"
+#include "EventAction.hh"
+#include "SteppingAction.hh"
+
+void ActionInitialization::Build() const {
+    auto runAction = new RunAction();
+    SetUserAction(runAction);
+    auto eventAction = new EventAction(runAction);
+    SetUserAction(eventAction);
+    SetUserAction(new PrimaryGeneratorAction());
+    SetUserAction(new SteppingAction(eventAction));
+}
 
