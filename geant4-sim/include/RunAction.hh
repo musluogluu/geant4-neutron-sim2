fa32f89b52d15d3ca1761b1f3713e817ffe9d28a
muslu <musluoglu.mert10@gmail.com>
2025-04-05 19:22:08 +0300

    Update RunAction.hh

--- geant4-sim/include/RunAction.hh
+++ geant4-sim/include/RunAction.hh
@@ -1,1 +1,25 @@
+#ifndef RUN_ACTION_HH
+#define RUN_ACTION_HH
+
+#include "G4UserRunAction.hh"
+#include <fstream>
+#include <vector>
+
+class RunAction : public G4UserRunAction {
+public:
+    RunAction();
+    ~RunAction() override;
+
+    void BeginOfRunAction(const G4Run*) override;
+    void EndOfRunAction(const G4Run*) override;
+
+    void AddNeutron(G4double angle, G4double energy);
+
+private:
+    std::ofstream outfile;
+    std::vector<int> angleBins;
+    std::vector<G4double> energies;
+};
+
+#endif
 
