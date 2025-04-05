09cca128571c201c8f905b213392860ac0dfac4f
muslu <musluoglu.mert10@gmail.com>
2025-04-05 19:21:34 +0300

    Update PrimaryGeneratorAction.cc

--- geant4-sim/src/PrimaryGeneratorAction.cc
+++ geant4-sim/src/PrimaryGeneratorAction.cc
@@ -1,1 +1,20 @@
+#include "PrimaryGeneratorAction.hh"
+#include "G4Proton.hh"
+#include "G4SystemOfUnits.hh"
+
+PrimaryGeneratorAction::PrimaryGeneratorAction() {
+    fParticleGun = new G4ParticleGun(1);
+    fParticleGun->SetParticleDefinition(G4Proton::ProtonDefinition());
+    fParticleGun->SetParticleEnergy(1*GeV);
+    fParticleGun->SetParticlePosition(G4ThreeVector(0, 0, -20*cm));
+    fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0, 0, 1));
+}
+
+PrimaryGeneratorAction::~PrimaryGeneratorAction() {
+    delete fParticleGun;
+}
+
+void PrimaryGeneratorAction::GeneratePrimaries(G4Event* event) {
+    fParticleGun->GeneratePrimaryVertex(event);
+}
 
