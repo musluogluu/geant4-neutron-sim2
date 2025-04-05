be72739acb51701ba16a7a7f6550941fb2a3305a
muslu <musluoglu.mert10@gmail.com>
2025-04-05 19:23:11 +0300

    Update SteppingAction.cc

--- geant4-sim/src/SteppingAction.cc
+++ geant4-sim/src/SteppingAction.cc
@@ -1,1 +1,22 @@
+#include "SteppingAction.hh"
+#include "EventAction.hh"
+#include "G4Track.hh"
+#include "G4Neutron.hh"
+#include "G4SystemOfUnits.hh"
+
+SteppingAction::SteppingAction(EventAction* eventAction) : fEventAction(eventAction) {}
+
+void SteppingAction::UserSteppingAction(const G4Step* step) {
+    auto track = step->GetTrack();
+    if (track->GetDefinition() == G4Neutron::NeutronDefinition()) {
+        auto postVol = step->GetPostStepPoint()->GetTouchableHandle()->GetVolume();
+        if (postVol && postVol->GetName() == "World") {
+            auto momDir = track->GetMomentumDirection();
+            auto angle = std::acos(momDir.z()) * 180. / CLHEP::pi;
+            auto energy = track->GetKineticEnergy();
+            fEventAction->GetRunAction()->AddNeutron(angle, energy);
+            track->SetTrackStatus(fStopAndKill); // Takip etmeye devam etme
+        }
+    }
+}
 
