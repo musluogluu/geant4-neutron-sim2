a6858cee1a971db088452e02ee6b0f46838ea000
muslu <musluoglu.mert10@gmail.com>
2025-04-05 19:21:23 +0300

    Update PrimaryGeneratorAction.hh

--- geant4-sim/include/PrimaryGeneratorAction.hh
+++ geant4-sim/include/PrimaryGeneratorAction.hh
@@ -1,1 +1,18 @@
+#ifndef PRIMARY_GENERATOR_ACTION_HH
+#define PRIMARY_GENERATOR_ACTION_HH
+
+#include "G4VUserPrimaryGeneratorAction.hh"
+#include "G4ParticleGun.hh"
+
+class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction {
+public:
+    PrimaryGeneratorAction();
+    ~PrimaryGeneratorAction() override;
+    void GeneratePrimaries(G4Event* event) override;
+
+private:
+    G4ParticleGun* fParticleGun;
+};
+
+#endif
 
