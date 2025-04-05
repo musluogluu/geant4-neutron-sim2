91397bc55b7155924841ff585eca65e360d82da3
muslu <musluoglu.mert10@gmail.com>
2025-04-05 19:22:53 +0300

    Update EventAction.cc

--- geant4-sim/src/EventAction.cc
+++ geant4-sim/src/EventAction.cc
@@ -1,1 +1,9 @@
+#include "EventAction.hh"
+#include "RunAction.hh"
+
+EventAction::EventAction(RunAction* runAction) : fRunAction(runAction) {}
+
+RunAction* EventAction::GetRunAction() const {
+    return fRunAction;
+}
 
