d0f9444c296d9f643775b405edaa96d7d2991025
muslu <musluoglu.mert10@gmail.com>
2025-04-05 19:20:56 +0300

    Update DetectorConstruction.hh

--- geant4-sim/include/DetectorConstruction.hh
+++ geant4-sim/include/DetectorConstruction.hh
@@ -1,1 +1,12 @@
+#ifndef DETECTOR_CONSTRUCTION_HH
+#define DETECTOR_CONSTRUCTION_HH
+
+#include "G4VUserDetectorConstruction.hh"
+
+class DetectorConstruction : public G4VUserDetectorConstruction {
+public:
+    G4VPhysicalVolume* Construct() override;
+};
+
+#endif
 
