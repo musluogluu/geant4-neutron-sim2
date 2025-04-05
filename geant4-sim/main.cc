fe0a9a91f6fb34c56747d7fdd79883f981676dda
muslu <musluoglu.mert10@gmail.com>
2025-04-05 19:20:45 +0300

    Update main.cc

--- geant4-sim/main.cc
+++ geant4-sim/main.cc
@@ -1,1 +1,20 @@
+#include "G4RunManagerFactory.hh"
+#include "G4UImanager.hh"
+#include "QGSP_BERT.hh"
+
+#include "DetectorConstruction.hh"
+#include "ActionInitialization.hh"
+
+int main() {
+    auto* runManager = G4RunManagerFactory::CreateRunManager(G4RunManagerType::Default);
+    runManager->SetUserInitialization(new DetectorConstruction());
+    runManager->SetUserInitialization(new QGSP_BERT);
+    runManager->SetUserInitialization(new ActionInitialization());
+    runManager->Initialize();
+
+    G4UImanager::GetUIpointer()->ApplyCommand("/run/beamOn 10000");
+
+    delete runManager;
+    return 0;
+}
 
